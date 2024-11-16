6f5544badcc88408bdda5ecf63e06c47ac02c13d
Jason <jasonkwm13@gmail.com>
2024-11-17 07:09:03 +0800

    .

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar = () => {
               />
             </button>
             <span
-              className="text-sm font-mono bg-gray-700 px-10 py-2 md:px-4 rounded"
+              className="text-sm font-mono bg-gray-700 px-10 py-2 md:px-4 rounded font-normal"
               onClick={() => {
                 navigator.clipboard.writeText(smartWalletAddress);
                 alert("Address copied!");
