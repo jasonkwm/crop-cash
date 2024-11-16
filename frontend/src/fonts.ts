a98664bd378cb1030163814cd1f4b4357ecc708f
Jason <jasonkwm13@gmail.com>
2024-11-16 12:28:24 +0800

    fix: build issue

--- frontend/src/fonts.ts
+++
@@ -1,6 +0,0 @@
-import "@fontsource/luckiest-guy"; // Defaults to 400 weight
-import "@fontsource/fredoka/300.css"; // Weight 300
-import "@fontsource/fredoka/400.css"; // Weight 400
-import "@fontsource/fredoka/500.css"; // Weight 500
-import "@fontsource/fredoka/600.css"; // Weight 600
-import "@fontsource/fredoka/700.css"; // Weight 700
